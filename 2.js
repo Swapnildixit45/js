6b709cc16a4d28ed7aefa0113ee2ed3df392075f
Swapnildixit45 <57413050+Swapnildixit45@users.noreply.github.com>
2022-04-08 14:59:02 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,12 @@
+var num1 = parseInt(prompt("Enter the first number"));
+var num2 = parseInt(prompt("Enter the second number"));
+
+var sum = num1 + num2;
+
+if(sum%2==0){
+    console.log("The sum is even");
+}
+else
+{
+    console.log("The sum is odd")
+}
